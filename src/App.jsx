087eb8873a91690bd5e39297263a5369e079c582
Adnan Pranto <prantoshikdar02@gmail.com>
2026-03-03 11:23:36 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Countries from './contries/Countries'
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1>React on the goo.....</h1>
+      <Countries></Countries>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/contries/Countries.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+           <h1>New countries</h1> 
+        </div>
+    );
+};
+export default Countries;
